3707f1ff50f58a91cc514a87bc2b986c905f2e9c
Dan <hello@danrowden.com>
2024-09-19 12:14:56 +0300

    Parameters for `events->send()`

--- src/Events.php
+++ src/Events.php
@@ -14,8 +14,22 @@ class Events
         $this->client = $client;
     }
 
-    public function send(array $payload)
+    public function send(string $event_name, string $email = null, string $user_id = null, array $contact_properties = [], array $event_properties = [], array $mailing_lists = [])
     {
+        if (!$email && !$user_id) {
+            throw new \InvalidArgumentException('You must provide an email or user_id value.');
+        }
+
+        $payload = [
+            'eventName' => $event_name,
+            'email' => $email,
+            'userId' => $user_id,
+            'eventProperties' => $event_properties,
+            'mailingLists' => $mailing_lists,
+        ];
+
+        $payload = array_merge($payload, $contact_properties);
+
         try {
             $response = $this->client->post('v1/events/send', ['json' => $payload]);
             return json_decode($response->getBody()->getContents(), true);
